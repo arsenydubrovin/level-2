26804fa14fbf7fe789a636db75b521d298958f98
Arseny Dubrovin <arsenydubrovin@arsenydubrovin.ru>
2024-02-11 18:32:29 +0300

    Add a program

--- develop/dev01/go.mod
+++ develop/dev01/go.mod
@@ -1,3 +1,9 @@
 module github.com/arsenydubrovin/level-2/tree/master/develop/dev01
 
 go 1.21.6
+
+require (
+	github.com/beevik/ntp v1.3.1 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -12,5 +12,24 @@ package main
 Программа должна проходить проверки go vet и golint.
 */
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
+	r, err := ntp.Query("0.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to request time data: %s\n", err)
+		os.Exit(1)
+	}
+
+	currentTime := time.Now()
+	exactTime := currentTime.Add(r.ClockOffset)
+
+	fmt.Printf("Текущее время: %s\n", currentTime.Format(time.UnixDate))
+	fmt.Printf("Точное время: %s\n", exactTime.Format(time.UnixDate))
 }
