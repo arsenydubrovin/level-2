10666043ead1e73991e0b8671018a5dee3d78317
Arseny Dubrovin <arsenydubrovin@arsenydubrovin.ru>
2024-02-16 08:54:15 +0300

    dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -18,6 +26,94 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+// builder добавляет к strings.Builder метод writeRunes
+type builder struct {
+	strings.Builder
+}
 
+// writeRunes записывает в builder n рун c
+func (b *builder) writeRunes(c rune, n int) {
+	b.Grow(n)
+
+	for i := 0; i < n; i++ {
+		b.WriteRune(c)
+	}
+}
+
+func main() {
+	v, err := unpackString("a4bc2d5e")
+	fmt.Printf("%q %v\n", v, err)
+
+	v, err = unpackString("abcd")
+	fmt.Printf("%q %v\n", v, err)
+
+	v, err = unpackString("45")
+	fmt.Printf("%q %v\n", v, err)
+
+	v, err = unpackString("")
+	fmt.Printf("%q %v\n", v, err)
+}
+
+func unpackString(str string) (string, error) {
+	var prevChar rune
+	var cnt int
+
+	var b builder
+
+	for _, c := range str {
+		// Если цифра, увеличение счётчика
+		if unicode.IsDigit(c) {
+			if prevChar == 0 {
+				return "", errors.New("(некорректная строка)")
+			}
+
+			err := increaseCounter(&cnt, c)
+			if err != nil {
+				return "", nil
+			}
+
+			continue
+		}
+
+		if prevChar != 0 {
+			if cnt == 0 {
+				cnt++ // чтобы записывать символы без числа подсле них
+			}
+
+			b.writeRunes(prevChar, cnt)
+			cnt = 0
+		}
+
+		prevChar = c
+	}
+
+	// Обработка последнего символа
+	if prevChar != 0 {
+		if unicode.IsDigit(prevChar) {
+			err := increaseCounter(&cnt, prevChar)
+			if err != nil {
+				return "", err
+			}
+		}
+
+		if cnt == 0 {
+			cnt++ // чтобы записывать символы без числа подсле них
+		}
+
+		b.writeRunes(prevChar, cnt)
+	}
+
+	return b.String(), nil
+}
+
+// increaseCounter добавляет цифру c в конец числа cnt
+func increaseCounter(cnt *int, c rune) error {
+	n, err := strconv.Atoi(string(c))
+	if err != nil {
+		return err
+	}
+
+	*cnt = *cnt*10 + n
+
+	return nil
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnpackString(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected string
+		err      error
+	}{
+		{"a4bc2d5e", "aaaabccddddde", nil},
+		{"abcd", "abcd", nil},
+		{"45", "", errors.New("(некорректная строка)")},
+		{"", "", nil},
+	}
+
+	for _, tc := range testCases {
+		result, err := unpackString(tc.input)
+
+		assert.Equal(t, tc.expected, result, "ввод: %s", tc.input)
+
+		if tc.err != nil {
+			assert.EqualError(t, err, tc.err.Error(), "ввод: %s", tc.input)
+		} else {
+			assert.NoError(t, err, "ввод: %s", tc.input)
+		}
+	}
+}
